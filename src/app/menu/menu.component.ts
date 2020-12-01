c8c3fec5573e3ddaa19d2854a246cb7affd9a536
Noé Esparsa <35199957+Rangoow@users.noreply.github.com>
2020-12-01 20:35:47 +0100

    Angular ninja template exercice complete

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pr-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+
+  navbarCollapsed = true;
+
+  toggleNavbar(): void {
+    switch (this.navbarCollapsed){
+      case true: {
+        this.navbarCollapsed = false;
+        break;
+      }
+      case false: {
+        this.navbarCollapsed = true;
+        break;
+      }
+    }
+  }
+}

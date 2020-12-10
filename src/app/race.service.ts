a776e83292fca62e0357e3643891c3eb179b9b6c
Noé Esparsa <35199957+Rangoow@users.noreply.github.com>
2020-12-10 22:00:08 +0100

    observable exercices

--- src/app/race.service.ts
+++ src/app/race.service.ts
@@ -1,23 +1,18 @@
 import { Injectable } from '@angular/core';
-import {RaceModel} from './races/models/race.model';
+import {RaceModel} from './models/race.model';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RaceService {
-
-  constructor() {
-  }
-
-  races: Array<RaceModel> = [
-    {name: 'Lyon'},
-    {name: 'Los Angeles'},
-    {name: 'Sydney'},
-    {name: 'Tokyo'},
-    {name: 'Casablanca'}
-  ];
-
-  list(): Array<RaceModel> {
-    return this.races;
+  list(): Observable<Array<RaceModel>> {
+    return of([{ name: 'Lyon' },
+      { name: 'Los Angeles' },
+      { name: 'Sydney' },
+      { name: 'Tokyo' },
+      { name: 'Casablanca' }]).pipe(delay(500));
   }
 }
--- src/app/races/races.component.ts
+++ src/app/races/races.component.ts
@@ -1,19 +1,18 @@
-import {RaceModel} from './models/race.model';
 import {RaceService} from '../race.service';
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RaceModel} from '../models/race.model';
 
 @Component({
   selector: 'pr-races',
   templateUrl: './races.component.html',
   styleUrls: ['./races.component.css']
 })
-export class RacesComponent {
-
-  constructor(private raceService: RaceService) { }
-
+export class RacesComponent implements OnInit {
   races: Array<RaceModel> = [];
 
-  list(): Array<RaceModel>{
-    return this.races = this.raceService.list();
+  constructor(private raceService: RaceService) {}
+
+  ngOnInit(): void {
+    this.raceService.list().subscribe(races => (this.races = races));
   }
 }
